b6d9e1d2a3caed487605266ea31e465cd8aa41e8
Danuarta Wiratama <danuarta.wiratama@gdn-commerce.com>
2025-04-24 12:18:10 +0700

    react

--- frontend/src/pages/DeepTrackPage.jsx
+++ frontend/src/pages/DeepTrackPage.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+
 
 export default function DeepTrackPage() {
   const { id } = useParams();
